c4dccff0d3f288b394da275d4c004d86e8837061
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2022-01-21 20:57:09 +0100

    Add h2 to pom and also add application properties

--- apiary/src/main/java/com/pk/App.java
+++ apiary/src/main/java/com/pk/App.java
@@ -1,13 +1,13 @@
 package com.pk;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 }
---
+++ apiary/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO ACCOUNT (LOGIN, PASSWORD, EMAIL, PRIVILEGES) VALUES ('admin', 'admin', 'admin@admin.com', 'admin');
+INSERT INTO ACCOUNT (LOGIN, PASSWORD, EMAIL, PRIVILEGES) VALUES ('user1', 'user1', 'user1@user1.com', 'worker');
+INSERT INTO ACCOUNT (LOGIN, PASSWORD, EMAIL, PRIVILEGES) VALUES ('user2', 'user2', 'user2@user2.com', 'beekeeper');
+INSERT INTO ACCOUNT (LOGIN, PASSWORD, EMAIL, PRIVILEGES) VALUES ('user3', 'user3', 'user3@user3.com', 'worker');
+INSERT INTO APIARY (LOCALIZATION, INFORMATION) VALUES ('Krakow', 'Some interesting apiary');
+INSERT INTO APIARY (INFORMATION) VALUES ('This one is only for information');
+INSERT INTO APIARY (LOCALIZATION) VALUES ('Lubzina');
+INSERT INTO EVENT (ID_apiary, Time_start, note) VALUES (1, CURRENT_TIMESTAMP, 'There was event');
+INSERT INTO EVENT (ID_apiary, Time_start, note) VALUES (3, CURRENT_TIMESTAMP, 'Bear attacked!');
+INSERT INTO ACCOUNT_APIARY (ID_account, ID_apiary) SELECT ID, 1 FROM ACCOUNT WHERE LOGIN = 'admin';
+INSERT INTO ACCOUNT_APIARY (ID_account, ID_apiary) SELECT ID, 2 FROM ACCOUNT WHERE LOGIN = 'admin';
+INSERT INTO ACCOUNT_APIARY (ID_account, ID_apiary) SELECT ID, 3 FROM ACCOUNT WHERE LOGIN = 'user1';
